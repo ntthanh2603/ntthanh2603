958e72eaea57ac5a4cd1f080c165de2937513a1b
Liam-2603 <tuanthanh2kk4@gmail.com>
2025-03-15 04:45:52 +0700

    update

--- dsa/hello_world.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var readlineSync = require("readline-sync");
-// Nhập vào một số từ người dùng
-var number = readlineSync.questionInt('Nhập vào một số: ');
-// In ra số đã nhập
-console.log("S\u1ED1 b\u1EA1n \u0111\u00E3 nh\u1EADp l\u00E0: ".concat(number));
--- dsa/hello_world.ts
+++
@@ -1,7 +0,0 @@
-import * as readlineSync from 'readline-sync';
-
-// Nhập vào một số từ người dùng
-const number: number = readlineSync.questionInt('Nhập vào một số: ');
-
-// In ra số đã nhập
-console.log(`Số bạn đã nhập là: ${number}`);
